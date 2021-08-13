38161c0da0bc4d474ee29c919a2488d7d3e3b934
Cristian Serrano <nanonathan.123@gmail.com>
2021-08-12 21:42:52 -0300

    commit ejercicio A

---
+++ tp2-agentes-racionales/code/Environment.py
@@ -0,0 +1,22 @@
+class Environment:
+    def __init__(self, sizeX = 2, sizeY = 2, init_posX = 0, init_posY = 0, dirt_rate = 0.1):
+        self.performance = 0;
+        self.board = [ [0] * sizeX ] * sizeY;
+
+        self.sizeX = sizeX;
+        self.sizeY = sizeY;
+        self.init_posX = init_posX;
+        self.init_posY = init_posY;
+        self.dirt_rate = dirt_rate;
+        
+
+#def acceptAction(self,action):
+
+#def isDirty(self):
+
+def getPerformance(self): 
+    return self.performance;
+
+
+#def printEnvironment(self):
+
