729f65cc18b42633bf85f9535ae0f31e16a595b2
Cristian Serrano <nanonathan.123@gmail.com>
2021-08-23 19:16:44 -0300

    init

---
+++ tp3-busquedas-no-informadas/code/Agente.py
@@ -0,0 +1,46 @@
+import random
+class Agent:
+    def __init__(self, env): #recibe como parámetro un objeto de la clase Environment
+        #Define el tamaño del tablero
+        self.maxX = env.sizeX - 1;
+        self.maxY = env.sizeY - 1;
+        #Define una posicion aleatoria
+        x = random.randrange(env.sizeX);
+        y = random.randrange(env.sizeY);
+        while (env.isDirty(x, y)): #Si hay obstáculo, sigue buscando una posicion al azar
+            x = random.randrange(env.sizeX);
+            y = random.randrange(env.sizeY);
+        self.currentX = x;
+        self.currentY = y;
+        #Avisa al tablero donde se va a encontrar
+        env.move(self.currentX, self.currentY);
+        #Guarda el entorno
+        self.env = env;
+
+    #Acciones del agente
+    def up(self):
+        #Se mueve
+        self.currentY -= 1;
+        #Envia la nueva posicion 
+        self.__sendPosition();
+    def down(self):
+        #Se mueve
+        self.currentY += 1;
+        #Envia la nueva posicion 
+        self.__sendPosition();
+    def left(self):
+        #Se mueve
+        self.currentX -= 1;
+        #Envia la nueva posicion 
+        self.__sendPosition();
+    def right(self):
+        #Se mueve
+        self.currentX += 1;
+        #Envia la nueva posicion 
+        self.__sendPosition();
+    def __sendPosition (self): 
+        self.env.move(self.currentX, self.currentY);
+
+    def breadthFirstSearch (self):
+        
+        return
---
+++ tp3-busquedas-no-informadas/code/Environment.py
@@ -0,0 +1,52 @@
+import random
+class Environment:
+    def __init__(self, sizeX = 100, sizeY = 100, dirt_rate = 0.5):
+        #Define e inicia el tablero
+        t = [];
+        for _ in range (sizeX):
+            t.append([0] * sizeY);
+        self.board = t;
+        #Guarda el tamaño del tablero
+        self.sizeX = sizeX;
+        self.sizeY = sizeY;
+        #Ensucia el tablero con obstaculos
+        self._initBoard(int(dirt_rate * 10));
+        #Le busca una posicion al objetivo
+        self.board[random.randrange(sizeX)][random.randrange(sizeY)] = -1;
+    
+    def _initBoard(self, dirtRate):
+        #Se genera un arreglo que guarda la probabilidad de que un cuadrado esté sucio
+        aRate = ([True] * dirtRate) + ([False] * (10 - dirtRate));
+        #Ensucia algunos cuadros del tablero
+        for i in range (self.sizeY): #Filas
+            for j in range (self.sizeX): #Columnas
+                if(aRate[random.randrange(10)]): #Si en la posicion es True, la casilla está sucia
+                    self.board[j][i] = 1;
+
+    def isDirty(self):
+        return self.board[self.posX][self.posY] == 1;
+
+    def move(self, x, y):
+        self.posX = x;
+        self.posY = y;
+
+    def printEnvironment(self):
+        #Arreglo que guardará la cantidad de filas que hay en el casillero
+        aBoard = [""] * len(self.board[0]);
+        #Se generan los strings de cada fila
+        for i in range (self.sizeY): #Filas
+            for j in range (self.sizeX): #Columnas
+                if(self.board[j][i] == 1): #Casillero Sucio
+                    aBoard[i] += " [---] |";
+                elif (self.board[j][i] == -1): #Objetivo
+                    aBoard[i] += "   X   |";
+                else: # Casillero limpio
+                    aBoard[i] += "       |";
+            #Elimina el ultimo '|' de la columna
+            aBoard[i] = aBoard[i][: len(aBoard[i]) - 1];
+        #Agrega al tablero el agente
+        aBoard[self.posY] = aBoard[self.posY][:8*self.posX] + " AGENT " + aBoard[self.posY][8*self.posX + 7:];
+        #Se muestra por pantalla el tablero
+        for sRow in aBoard:
+            print(sRow);
+        print("");
